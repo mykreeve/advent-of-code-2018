59fe8d94814bd3c5ba291f2db89cf07f09e567f8
Michael <myk@Michaels-MacBook-Pro.local>
2018-12-27 23:20:20 +0000

    final fix for day 17

--- day17.py
+++ day17.py
@@ -34,12 +34,23 @@ for line in file:
             earth[x,y]='#'
 
 earth[500,0]='|'
+latest_change=(500,0)
 
-# def do_print(earth):
-#     for y in range(240,280):
-#         for x in range(530,580):
-#             print (earth[x,y], end="")
-#         print ("", end="\n")
+def do_print(earth, latest_change):
+    for y in range(latest_change[1]-30,latest_change[1]+30):
+        for x in range(latest_change[0]-60,latest_change[0]+40):
+            if (x,y) in earth:
+                print (earth[x,y], end="")
+            else:
+                print ("=", end="")
+        print ("", end="\n")
+    # for y in range(170,190):
+    #     for x in range(520,550):
+    #         if (x,y) in earth:
+    #             print (earth[x,y], end="")
+    #         else:
+    #             print ("=", end="")
+    #     print ("", end="\n")
 
 def count_waters(earth):
     got_to=0
@@ -57,10 +68,10 @@ def count_waters(earth):
 
 step=0
 while True:
-    # do_print(earth)
     if step%10==0:
+        do_print(earth, latest_change)
         k = count_waters(earth)
-        print (str(k[0]) + " standing, " + str(k[1]) + " waters - got to " + str(k[2]) + " out of " + str(maxY-1) )
+        print ("--STATUS UPDATE --- " + str(k[0]) + " standing, " + str(k[1]) + " waters - got to " + str(k[2]) + " out of " + str(maxY-1) )
 
     for y in range(minY, maxY):
         for x in range(minX, maxX):
@@ -70,6 +81,15 @@ while True:
                     if earth[x,y+1]=='.' or earth[x,y+1]=='|':
                         print ("filling gap " + str(x) + "," + str(y+1))
                         earth[x,y+1]='~'
+                        latest_change=(x,y+1)
+                if (x+1,y) in earth:
+                    if earth[x+1,y]=='.':
+                        print ("filling gap " + str(x+1) + "," + str(y))
+                        earth[x+1,y]='~'
+                if (x-1,y) in earth:
+                    if earth[x-1,y]=='.':
+                        print ("filling gap " + str(x-1) + "," + str(y))
+                        earth[x-1,y]='~'
             if earth[x,y]=='|':
                 if (x,y+1) in earth:
                     if earth[x,y+1]=='.':
@@ -94,6 +114,7 @@ while True:
                                 min -= 1
                             for i in range(min+1,max):
                                 earth[i,y]='~'
+                            latest_change=(max,y)
                     if earth[x,y+1]=='~':
                         # water hits standing water
                         if earth[x-1,y]=='|' and earth[x+1,y]=='|':
@@ -114,19 +135,23 @@ while True:
                                 print ("contained in clay " + str(min+1) + "," + str(y) + " - " + str(max) + "," + str(y))
                                 for i in range(min+1,max):
                                     earth[i,y]='~'
+                                latest_change=(max,y)
                             # contained in clay on left only
                             if earth[min,y]=='#' and earth[max,y]!='#':
                                 print ("contained on left only " + str(min+1) + "," + str(y) + " - " + str(max+1) + "," + str(y))
                                 for i in range(min+1,max+1):
                                     earth[i,y]='|'
+                                latest_change=(max+1,y)
                             # contained in clay on right only
                             if earth[min,y]!='#' and earth[max,y]=='#':
                                 print ("contained on right only " + str(min) + "," + str(y) + " - " + str(max) + "," + str(y))
                                 for i in range(min,max):
-                                    earth[i,y]='|'
+                                    earth[i,y]='|'                        
+                                latest_change=(max,y)
                             # unconstrained flow
                             if earth[min,y]!='#' and earth[max,y]!='#':
                                 print ("unconstrained " + str(min) + "," + str(y) + " - " + str(max+1) + "," + str(y))
                                 for i in range(min,max+1):
-                                    earth[i,y]='|'
+                                    earth[i,y]='|'                      
+                                latest_change=(max+1,y)
     step+=1
